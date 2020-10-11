a634507e8e1920bfe6297109d8ca16a1d8c3aa83
Yafit Katz <yafit22092000@gmail.com>
2020-10-11 16:48:08 +0300

    Client side redux without SAGA

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,28 +1,83 @@
 /**
  *
- * App.js
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
+ * App
  *
  */
 
-import React from 'react';
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { useInjectReducer } from 'utils/injectReducer';
+
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
+import HomePage from 'components/HomePage/Loadable';
+import AddMeetingPage from 'containers/AddMeetingPage';
+import MeetingsDashboard from 'components/MeetingsDashboard';
+import CalendarPage from 'containers/CalendarPage';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-
+import reducer from './reducer';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import { makeSelectMeetingEvent, makeSelectMeetingCard } from './selectors';
 import 'style.scss';
 
-export default function App() {
+export function App(props) {
+  useInjectReducer({ key: 'app', reducer });
+
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-      <GlobalStyle />
+      <center>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/addMeeting" component={() => <AddMeetingPage />} />
+          <Route
+            path="/meetingsDashboard"
+            component={() => (
+              <MeetingsDashboard meetingCards={props.meetingCard} />
+            )}
+          />
+          <Route
+            path="/calendar"
+            component={() => (
+              <CalendarPage meetingEvents={props.meetingEvent} />
+            )}
+          />
+          <Route component={NotFoundPage} />
+        </Switch>
+        <Footer />
+      </center>
     </div>
   );
 }
+
+App.propTypes = {
+  meetingEvent: PropTypes.array,
+  meetingCard: PropTypes.array,
+};
+
+const mapStateToProps = createStructuredSelector({
+  meetingEvent: makeSelectMeetingEvent(),
+  meetingCard: makeSelectMeetingCard(),
+});
+
+// onClick=(props.handleChange(meeting))
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(App);
--- app/containers/App/selectors.js
+++ app/containers/App/selectors.js
@@ -1,11 +1,51 @@
+// import React from 'react';
 import { createSelector } from 'reselect';
+import { initialState } from './reducer';
 
-const selectRouter = state => state.router;
+/**
+ * Direct selector to the app state domain
+ */
 
-const makeSelectLocation = () =>
+const selectMeeting = state => state.meeting || initialState;
+
+/**
+ * Other specific selectors
+ */
+
+/**
+ * Default selector used by App
+ */
+
+const makeSelectMeetingEvent = () =>
   createSelector(
-    selectRouter,
-    routerState => routerState.location,
+    selectMeeting,
+    substate =>
+      [] &&
+      substate.map(meeting => ({
+        id: meeting.id,
+        title: `${meeting.firstParticipant.firstName}
+         ${meeting.firstParticipant.lastName} - 
+          ${meeting.secondParticipant.firstName}
+          ${meeting.secondParticipant.lastName}`,
+        start: meeting.date,
+      })),
   );
 
-export { makeSelectLocation };
+const makeSelectMeetingCard = () =>
+  createSelector(
+    selectMeeting,
+    substate =>
+      [] &&
+      substate.map(meeting => ({
+        id: meeting.id,
+        firstParticipant: `${meeting.firstParticipant.firstName} 
+        ${meeting.firstParticipant.lastName}`,
+        secondParticipant: `${meeting.secondParticipant.firstName} 
+        ${meeting.secondParticipant.lastName}`,
+        date: meeting.date,
+        address: meeting.address,
+      })),
+  );
+
+export { selectMeeting, makeSelectMeetingEvent };
+export { makeSelectMeetingCard };
