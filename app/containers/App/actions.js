9ac66f97d8b6ca3e966cc87c8016d18b9d7640ae
yafitKatz <66747227+yafitKatz@users.noreply.github.com>
2020-10-14 12:36:19 +0300

    recreating global redux without saga

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -5,10 +5,26 @@
  */
 
 import {
+  ADD_MEETING,
+  DEL_MEETING,
   LOAD_MEETINGS,
   LOAD_MEETINGS_SUCCESS,
   LOAD_MEETINGS_ERROR,
 } from './constants';
+
+export function addMeeting(meeting) {
+  return {
+    type: ADD_MEETING,
+    payload: meeting,
+  };
+}
+
+export function delMeeting({ meeting }) {
+  return {
+    type: DEL_MEETING,
+    payload: meeting,
+  };
+}
 
 export function loadMeetings() {
   return {
