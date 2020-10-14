065182a31f7c313de3dfb1021c8c64283c7a4c5f
yafitKatz <66747227+yafitKatz@users.noreply.github.com>
2020-10-14 12:54:39 +0300

    recreating global redux without saga

--- app/containers/AddMeetingPage/reducer.js
+++ app/containers/AddMeetingPage/reducer.js
@@ -7,110 +7,23 @@ import { ADD_MEETING } from './constants';
 
 export const initialState = [
   {
-    id: '5f7b1a59316f8e6d61076240',
-    address: '969 Doone Court, Spokane, Connecticut, 7811',
-    date: '2020-10-08',
+    id: '',
+    address: '',
+    date: new Date(),
     firstParticipant: {
-      firstName: 'Chelsea',
-      lastName: 'Gross',
-      phone: '+1 (812) 436-2719',
-      email: 'chelseagross@moreganic.com',
+      firstName: '',
+      lastName: '',
+      tel: '',
+      email: '',
     },
     secondParticipant: {
-      firstName: 'Rebecca',
-      lastName: 'Bailey',
-      phone: '+1 (909) 555-3203',
-      email: 'rebeccabailey@moreganic.com',
-    },
-  },
-  {
-    id: '5f7b1a597e8437af8f4557f8',
-    address: '850 Conway Street, Nettie, Hawaii, 5018',
-    date: '2020-10-04',
-    firstParticipant: {
-      firstName: 'Mcfadden',
-      lastName: 'Castaneda',
-      phone: '+1 (857) 465-2802',
-      email: 'mcfaddencastaneda@moreganic.com',
-    },
-    secondParticipant: {
-      firstName: 'Leah',
-      lastName: 'Mosley',
-      phone: '+1 (838) 442-2200',
-      email: 'leahmosley@moreganic.com',
-    },
-  },
-  {
-    id: '5f7b1a597654944ddd60188b',
-    address: '547 Hutchinson Court, Calverton, Iowa, 6516',
-    date: '2020-10-29',
-    firstParticipant: {
-      firstName: 'Melva',
-      lastName: 'Rivers',
-      phone: '+1 (803) 401-3942',
-      email: 'melvarivers@moreganic.com',
-    },
-    secondParticipant: {
-      firstName: 'Anita',
-      lastName: 'Caldwell',
-      phone: '+1 (828) 576-3770',
-      email: 'anitacaldwell@moreganic.com',
-    },
-  },
-  {
-    id: '5f7b1a59937ac89a099e1192',
-    address: '550 Pleasant Place, Sattley, Northern Mariana Islands, 7274',
-    date: '2020-10-08',
-    firstParticipant: {
-      firstName: 'Regina',
-      lastName: 'Guerra',
-      phone: '+1 (972) 406-2178',
-      email: 'reginaguerra@moreganic.com',
-    },
-    secondParticipant: {
-      firstName: 'Ashlee',
-      lastName: 'Hester',
-      phone: '+1 (956) 518-2529',
-      email: 'ashleehester@moreganic.com',
-    },
-  },
-  {
-    id: '5f7b1a597dc8b4c131dd7aa5',
-    address: '520 Bennet Court, Hilltop, Alabama, 5930',
-    date: '2020-10-08',
-    firstParticipant: {
-      firstName: 'Adkins',
-      lastName: 'Mccoy',
-      phone: '+1 (898) 414-3292',
-      email: 'adkinsmccoy@moreganic.com',
-    },
-    secondParticipant: {
-      firstName: 'Monica',
-      lastName: 'Dalton',
-      phone: '+1 (803) 418-2646',
-      email: 'monicadalton@moreganic.com',
+      firstName: '',
+      lastName: '',
+      tel: '',
+      email: '',
     },
   },
 ];
-// [
-//   {
-//     id: '',
-//     address: '',
-//     date: new Date(),
-//     firstParticipant: {
-//       firstName: '',
-//       lastName: '',
-//       tel: '',
-//       email: '',
-//     },
-//     secondParticipant: {
-//       firstName: '',
-//       lastName: '',
-//       tel: '',
-//       email: '',
-//     },
-//   },
-// ];
 
 /* eslint-disable default-case, no-param-reassign */
 const addMeetingReducer = (state = initialState, action) => {
