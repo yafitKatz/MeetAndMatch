c7fe2813d8fe1f9b881224a00f7debecff1e3a30
yafitKatz <66747227+yafitKatz@users.noreply.github.com>
2020-10-14 13:05:18 +0300

    recreating global redux without saga

--- app/containers/MeetingsDashboard/reducer.js
+++ app/containers/MeetingsDashboard/reducer.js
@@ -11,23 +11,25 @@ export const initialState = {};
 /* eslint-disable default-case, no-param-reassign */
 const meetingsDashboardReducer = (state = initialState, action) => {
   switch (action.type) {
-    case DEL_MEETING:
-      alert('deletedMeetingDashboard');
-      return state;
-    // ==========APPROACH 1:=============
-    // const newState = { state }.filter(
-    //   meeting => meeting.id !== action.payload,
-    // );
-    // return { ...newState };
-    // ==========APPROACH 2:=============
-    // return {
-    //   ...state,
-    //   meetings: state.meetings.filter(
-    //     meeting => meeting.id !== action.payload,
-    //   ),
-    // };
-    // ==========APPROACH 3:=============
-    // return { ...state, meetings: state.meetings.splice(action.payload, 1) };
+    case DEL_MEETING: {
+      break;
+      // ==========APPROACH 1:=============
+      //   debugger;
+      //   const newState = state.filter(
+      //     meeting => meeting.id !== action.payload.id,
+      //   );
+      //   return { ...newState };
+      // }
+      // ==========APPROACH 2:=============
+      // return {
+      //   ...state,
+      //   meetings: state.meetings.filter(
+      //     meeting => meeting.id !== action.payload,
+      //   ),
+      // };
+      // ==========APPROACH 3:=============
+      // return { ...state, meetings: meetings.splice(action.payload, 1) };
+    }
     default:
       return state;
   }
