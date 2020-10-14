cad3699be607ef38d2d2e400f5c6ce677f5bd531
yafitKatz <66747227+yafitKatz@users.noreply.github.com>
2020-10-14 12:32:30 +0300

    Update reducers.js

--- app/reducers.js
+++ app/reducers.js
@@ -7,21 +7,15 @@ import { connectRouter } from 'connected-react-router';
 
 import history from 'utils/history';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
-import loadingReducer from 'containers/App/reducer';
-import errorReducer from 'containers/App/reducer';
-import addMeetingReducer from './containers/AddMeetingPage/reducer';
-import meetingsDashboardReducer from './containers/MeetingsDashboard/reducer';
+import globalReducer from 'containers/App/reducer';
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
  */
 export default function createReducer(injectedReducers = {}) {
   const rootReducer = combineReducers({
-    meetings: addMeetingReducer,
-    meetingsDashboardReducer,
+    global: globalReducer,
     language: languageProviderReducer,
-    // isLoading: loadingReducer,
-    // error: errorReducer,
     router: connectRouter(history),
     ...injectedReducers,
   });
